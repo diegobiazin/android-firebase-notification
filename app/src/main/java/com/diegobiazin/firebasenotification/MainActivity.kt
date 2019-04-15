7949ae42af9022b5320eb0bc8bfc06545eec72eb
Diego Biazin <diego_biazin@hotmail.com>
2019-04-15 14:34:53 -0300

    EventListener

--- app/src/main/java/com/diegobiazin/firebasenotification/MainActivity.kt
+++ app/src/main/java/com/diegobiazin/firebasenotification/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.diegobiazin.firebasenotification.util.Util
 import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,30 +27,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View) {
         when (view.id) {
             R.id.switch_Sports -> {
-                val statusSports: Boolean = switch_Sports.isChecked
-
-                if (statusSports) {
-                    FirebaseMessaging.getInstance().subscribeToTopic("sports")
-                    Toast.makeText(this, "Inscrição Sports Realizada com Sucesso", Toast.LENGTH_LONG).show()
-                }
-                else {
-                    FirebaseMessaging.getInstance().unsubscribeFromTopic("sports")
-                    Toast.makeText(this, "Inscrição Sports Cancelada", Toast.LENGTH_LONG).show()
-                }
-
-
+                subscribeSport()
             }
             R.id.switch_Politica -> {
-                val statusPolitica: Boolean = switch_Politica.isChecked
-
-                if (statusPolitica) {
-                    FirebaseMessaging.getInstance().subscribeToTopic("politica")
-                    Toast.makeText(this, "Inscrição Política Realizada com Sucesso", Toast.LENGTH_LONG).show()
-                }
-                else {
-                    FirebaseMessaging.getInstance().unsubscribeFromTopic("sports")
-                    Toast.makeText(this, "Inscrição Política Cancelada", Toast.LENGTH_LONG).show()
-                }
+                subscribePolitica()
             }
             R.id.button_CadastrarToken -> {
                 Toast.makeText(this, "Button Cadastrar Token", Toast.LENGTH_LONG).show()
@@ -60,4 +41,66 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun subscribeSport() {
+        if (Util.statusInternet_MoWi(baseContext))
+            notificaSubscribeSport()
+        else
+            Toast.makeText(this, "Erro - Sem conexão com a internet - Verifique o Wifi ou 3G", Toast.LENGTH_LONG).show()
+    }
+
+    private fun notificaSubscribeSport() {
+        val statusSports: Boolean = switch_Sports.isChecked
+
+        if (statusSports) {
+            FirebaseMessaging.getInstance().subscribeToTopic("sports").addOnCompleteListener {
+                if (it.isComplete)
+                    Toast.makeText(this, "Inscrição Sports Realizada com Sucesso", Toast.LENGTH_LONG).show()
+                else {
+                    switch_Sports.isChecked = false
+                    Toast.makeText(this, "Erro - Inscrição Sports não sucedida", Toast.LENGTH_LONG).show()
+                }
+            }
+        } else {
+            FirebaseMessaging.getInstance().unsubscribeFromTopic("sports").addOnCompleteListener {
+                if (it.isComplete)
+                    Toast.makeText(this, "Inscrição Sports Cancelada com Sucesso", Toast.LENGTH_LONG).show()
+                else {
+                    switch_Sports.isChecked = false
+                    Toast.makeText(this, "Erro - Cancelamento Inscrição Sports não sucedida", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun notificaSubscribePolitica() {
+        val statusPolitica: Boolean = switch_Politica.isChecked
+
+        if (statusPolitica) {
+            FirebaseMessaging.getInstance().subscribeToTopic("politica").addOnCompleteListener {
+                if (it.isComplete)
+                    Toast.makeText(this, "Inscrição Politica Realizada com Sucesso", Toast.LENGTH_LONG).show()
+                else {
+                    switch_Politica.isChecked = false
+                    Toast.makeText(this, "Erro - Inscrição Politica não sucedida", Toast.LENGTH_LONG).show()
+                }
+            }
+        } else {
+            FirebaseMessaging.getInstance().unsubscribeFromTopic("politica").addOnCompleteListener {
+                if (it.isComplete)
+                    Toast.makeText(this, "Inscrição Politica Cancelada com Sucesso", Toast.LENGTH_LONG).show()
+                else {
+                    switch_Politica.isChecked = false
+                    Toast.makeText(this, "Erro - Cancelamento Inscrição Politica não sucedida", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
+    }
+
+    private fun subscribePolitica() {
+        if (Util.statusInternet_MoWi(baseContext))
+            notificaSubscribePolitica()
+        else
+            Toast.makeText(this, "Erro - Sem conexão com a internet - Verifique o Wifi ou 3G", Toast.LENGTH_LONG).show()
+    }
 }
